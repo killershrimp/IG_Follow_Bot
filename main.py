460f3593670316fadc2b909c08b1dd4d0b075fca
= <alexyun04@gmail.com>
2021-05-21 17:49:54 -0700

    fix some bugs

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from time import sleep
 from selenium import webdriver, common
 
+
+def alr_req(status):
+    return status == 'Following' or status == 'Message' or status == 'Requested' or status == 'Unfollow'
+
+
+def should_follow(status):
+    return status == 'Follow'
+
+
 browser = webdriver.Firefox()
 browser.implicitly_wait(5)
 
@@ -16,7 +25,7 @@ try:
 
         login_button = browser.find_element_by_xpath("//button[@type='submit']")
         login_button.click()
-    sleep(2)
+    sleep(5)
 
     # follow users
     unsuccessful = []
@@ -25,16 +34,21 @@ try:
             username = username.rstrip()
             if username != "":
                 browser.get('https://www.instagram.com/' + username + '/')
+                sleep(1)
                 try:
-                    followButton = browser.find_element_by_css_selector('button')
-                    followButton.click()
+                    follow_button = browser.find_elements_by_css_selector('button')
+                    for i in range(2):
+                        status = follow_button[i].text.rstrip()
+                        if should_follow(status):
+                            follow_button[i].click()
+                            break
                 except common.exceptions.NoSuchElementException:
                     # if user not found
                     unsuccessful.append(username)
+                except IndexError:
+                    unsuccessful.append(username)
 
     print("These users couldn't be found: " + str(unsuccessful))
-
-    sleep(5)
     browser.close()
 except common.exceptions.ElementClickInterceptedException:
     print("Add your username and password to login_info.txt and run again!")
